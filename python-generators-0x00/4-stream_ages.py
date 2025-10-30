849577eb5d28adf762a8b7422c56d5a2ad346cfb
TAREK <tareksayedmokhtar@yahoo.com>
2025-10-30 20:39:00 +0300

    Update 4-stream_ages.py

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,1 +1,29 @@
+import seed
+
+def stream_user_ages():
+    conn = seed.connect_to_prodev()
+    if not conn:
+        return
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT age FROM user_data;")
+        row = cur.fetchone()
+        while row:          # loop 1
+            yield int(row[0])
+            row = cur.fetchone()
+        cur.close()
+    finally:
+        conn.close()
+
+def compute_average_age():
+    total = 0
+    count = 0
+    for age in stream_user_ages():  # loop 2
+        total += age
+        count += 1
+    avg = (total / count) if count else 0
+    print(f"Average age of users: {avg:.2f}")
+
+if __name__ == "__main__":
+    compute_average_age()
 
