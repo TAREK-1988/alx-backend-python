d76ef1e1fc3e547fc2fa114eaa202399daa1e91d
TAREK <tareksayedmokhtar@yahoo.com>
2025-10-30 20:35:02 +0300

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,1 +1,27 @@
+from typing import Generator, List, Dict, Any
+import seed
+
+def stream_users_in_batches(batch_size: int) -> Generator[List[Dict[str, Any]], None, None]:
+    conn = seed.connect_to_prodev()
+    if not conn:
+        return
+    try:
+        cur = conn.cursor(dictionary=True)
+        cur.execute("SELECT user_id, name, email, age FROM user_data;")
+        batch = cur.fetchmany(size=batch_size)
+        while batch:
+            yield [
+                {"user_id": r["user_id"], "name": r["name"], "email": r["email"], "age": int(r["age"])}
+                for r in batch
+            ]
+            batch = cur.fetchmany(size=batch_size)
+        cur.close()
+    finally:
+        conn.close()
+
+def batch_processing(batch_size: int = 50) -> None:
+    for batch in stream_users_in_batches(batch_size):  # loop 1
+        for user in batch:                             # loop 2
+            if user["age"] > 25:
+                print(user)
 
