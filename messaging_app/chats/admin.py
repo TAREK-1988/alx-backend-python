4cdf54fae02a7aa72f073e99d18f50b1c4d8011f
TAREK <tareksayedmokhtar@yahoo.com>
2025-11-12 23:01:12 +0200

    Update admin.py

--- messaging_app/chats/admin.py
+++ messaging_app/chats/admin.py
@@ -1,1 +1,55 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User, Conversation, Message
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ("email", "role", "phone_number", "created_at")
+    ordering = ("-created_at",)
+    search_fields = ("email", "first_name", "last_name")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "phone_number")}),
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+        ("Role", {"fields": ("role",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "role"),
+            },
+        ),
+    )
+
+    filter_horizontal = ("groups", "user_permissions")
+
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ("conversation_id", "created_at")
+    search_fields = ("conversation_id",)
+    filter_horizontal = ("participants",)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("message_id", "sender", "conversation", "sent_at")
+    search_fields = ("message_body", "sender__email")
+    list_filter = ("sent_at",)
 
