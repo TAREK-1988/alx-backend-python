a7244329256778b52f318098a0d9e5b1f36fce0e
TAREK <tareksayedmokhtar@yahoo.com>
2025-11-12 23:00:09 +0200

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,1 +1,14 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsSenderOrReadOnly(BasePermission):
+    """
+    Allow read-only access to any request.
+    Allow write access only to the message sender.
+    """
+
+    def has_object_permission(self, request, view, obj) -> bool:
+        if request.method in SAFE_METHODS:
+            return True
+        return getattr(obj, "sender_id", None) == getattr(request.user, "user_id", None)
 
