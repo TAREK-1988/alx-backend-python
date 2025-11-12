163670c21463dad13459eebbe1e6e02200f1c3bf
TAREK <tareksayedmokhtar@yahoo.com>
2025-11-12 23:06:11 +0200

    Create tests.py

---
+++ messaging_app/chats/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class ChatsAppSmokeTest(TestCase):
+    """
+    Simple smoke test to ensure the chats app is correctly installed.
+    """
+
+    def test_true_is_true(self):
+        self.assertTrue(True)
