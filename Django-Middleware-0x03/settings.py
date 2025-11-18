b81b24cefd237b53af468e5f7a7f059399a3db84
TAREK <tareksayedmokhtar@yahoo.com>
2025-11-18 15:04:27 +0200

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -29,7 +29,9 @@ MIDDLEWARE = [
     "chats.middleware.RequestLoggingMiddleware",
     "chats.middleware.RestrictAccessByTimeMiddleware",
     "chats.middleware.OffensiveLanguageMiddleware",
+    "chats.middleware.RolepermissionMiddleware",
 ]
+
 
 
 
