0e418c86803ad82c6799d5c7e04eb6757c827ee6
TAREK <tareksayedmokhtar@yahoo.com>
2025-11-18 14:59:42 +0200

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -28,7 +28,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "chats.middleware.RequestLoggingMiddleware",
     "chats.middleware.RestrictAccessByTimeMiddleware",
+    "chats.middleware.OffensiveLanguageMiddleware",
 ]
+
 
 
 ROOT_URLCONF = "messaging_app.urls"
